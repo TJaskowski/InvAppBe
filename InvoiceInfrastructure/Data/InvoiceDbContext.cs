705c4c3da8c3aac505d91e6fea3cec2ecaf2baed
Tomasz J <tomasz.jaskowski@microsoft.wsei.edu.pl>
2025-07-05 00:37:53 +0200

    Entities properties added to dbContext

--- InvoiceInfrastructure/Data/InvoiceDbContext.cs
+++ InvoiceInfrastructure/Data/InvoiceDbContext.cs
@@ -8,4 +8,30 @@ public class InvoiceDbContext : DbContext
     public DbSet<Company> Companies { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Company>()
+        .OwnsOne(c => c.ContactDetails);
+        modelBuilder.Entity<Customer>()
+            .OwnsOne(c => c.ContactDetails);
+        modelBuilder.Entity<Invoice>()
+            .HasOne(i => i.Company)
+            .WithMany()
+            .HasForeignKey("CompanyId")
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Invoice>()
+            .HasOne(i => i.Customer)
+            .WithMany()
+            .HasForeignKey("CustomerId")
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Invoice>()
+            .HasMany(i => i.Products)
+            .WithMany()
+            .UsingEntity(j => j.ToTable("InvoiceProducts"));
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
